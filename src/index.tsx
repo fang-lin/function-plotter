c39cf44897e2b3fb796fd585dd828db3630f67f7
fanglin <fanglin.me@gmail.com>
2020-04-13 01:59:12 +0200

    Add more samples

--- src/index.tsx
+++ src/index.tsx
@@ -9,7 +9,7 @@ import {utoa} from './helpers/params';
 const dom = document.getElementById('root');
 if (dom) {
     const history = createHashHistory();
-    const defaultURL = `/12/0/0/0110110/0/${utoa('[["cos(51*x)*sin(x)","rgb(0, 136, 0)",true]]')}`;
+    const defaultURL = `/13/0/0/0110111/4/${utoa('[["sin(x)","rgb(238, 102, 0)",true],["-sin(x)","rgb(0, 136, 204)",true],["tan(x)","rgb(136, 34, 0)",true],["tan(-x)","rgb(238, 170, 0)",true],["cos(31*x)*sin(x)","rgb(0, 136, 0)",true]]')}`;
 
     render(<Router history={history}>
         <GlobalStyle/>
